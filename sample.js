0d4aad5d46528a04a3b03bceea1572df93280c05
nikhithakundar <nikhithamogaveera2002@gamil.com>
2022-10-14 19:51:35 +0530

    add all files into remote

---
+++ sample.js
@@ -0,0 +1,16 @@
+const number = prompt('Enter the number: ');
+const number2 = prompt('Enter the number: ');
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
+
+const number1 = 9;
+const number2 = 4;
+const number3 = 'hello';
+
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+const result3 = Math.sqrt(number3);
+
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
+console.log(`The square root of ${number3} is ${result3}`);
